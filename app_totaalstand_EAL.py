e6b2a65639944950e9945e24695a5ef27370a187
yannick-dartcounter <yannick.dartcounter@outlook.com>
2025-08-31 21:35:40 +0200

    Update app_totaalstand_EAL.py

--- app_totaalstand_EAL.py
+++ app_totaalstand_EAL.py
@@ -10,7 +10,7 @@ st.title("🏆 Total ranking – EAL League")
 # 📁 GitHub-bestand
 url = "https://raw.githubusercontent.com/yannick-dartcounter/EAL-League/main/totaalstand_EAL1_EAL8.xlsx"
 
-@st.cache_data(ttl=60)
+@st.cache_data(ttl=10)
 def laad_excel_van_github(url):
     response = requests.get(url)
     response.raise_for_status()
@@ -67,6 +67,7 @@ st.dataframe(
     use_container_width=True,
     height=len(df) * 35
 )
+
 
 
 
